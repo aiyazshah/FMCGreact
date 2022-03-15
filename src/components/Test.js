80027fb95f560bcc018e30b0ead86f0507ad355b
Aiyaz gani <shahaiyaz71@gmail.com>
2022-03-15 11:15:37 +0530

    tst

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,18 +1,19 @@
 import React, { useEffect, useState } from 'react'
 
- export default function Test() {
-//         const [count, setCount] = useState(0);
+export default function Test() {
+  //         const [count, setCount] = useState(0);
 
-// useEffect(()=>{setTimeout(()=>{setCount(count+1),1000})})
-// // useEffect(()=>{setTimeout(()=>{setCount((count)=>count + 1);},1000);});
-const [count, setCount] = useState(0);
+  // useEffect(()=>{setTimeout(()=>{setCount(count+1),1000})})
+  // // useEffect(()=>{setTimeout(()=>{setCount((count)=>count + 1);},1000);});
+  const [count, setCount] = useState(0);
 
-useEffect(()=>{setTimeout(()=>{setCount(count+1)},1000)},[count]);
+  useEffect(() => { setTimeout(() => { setCount(count + 1) }, 1000) }, [count]);
 
-    
-    return (
+
+  return (
     <div>
       {count}
+      {/* hello there */}
     </div>
   )
 }
